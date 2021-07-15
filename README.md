76251c14a592da84d7c2c87d1479ec07bef8b437
Silveira Guimaraes <silveira_oi@yahoo.com.br>
2021-07-15 15:12:42 +0000

    Primeiro commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-[![Gitpod ready-to-code](https://img.shields.io/badge/Gitpod-ready--to--code-blue?logo=gitpod)](https://gitpod.io/#https://github.com/rocketseat-education/ignite-template-conceitos-do-nodejs)
 
+- []Será permitida a criação de um usuário com name e username, bem como fazer o CRUD de todos:
+- [] Criar um novo *todo*;
+- [] Listar todos os *todos*;
+- [] Alterar o `title` e `deadline` de um *todo* existente;
+- [] Marcar um *todo* como feito;
+- [] Excluir um *todo*;
+- [] Tudo isso para cada usuário em específico (o `username`  será passado pelo header)
--- src/index.js
+++ src/index.js
@@ -1,21 +1,33 @@
 const express = require('express');
 const cors = require('cors');
-
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
   // Complete aqui
+  const { username } = request.aderhe
 }
+
+app.get("/", (request, response) =>{
+  response.json("Todo List").status(200);
+} )
 
 app.post('/users', (request, response) => {
   // Complete aqui
+  const {name, username} = request.body;
+  users.push({
+    id: uuidv4,
+    name,
+    username,
+    todos: [] 
+  });
+  return response.status(201).send();  
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
