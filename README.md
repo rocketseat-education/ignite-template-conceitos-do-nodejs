3cca722b27757c4fd78555873fefd598fa4fc5e2
Silveira Guimaraes <silveira_oi@yahoo.com.br>
2021-07-15 16:52:48 +0000

    Continua...

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 
-- []Será permitida a criação de um usuário com name e username, bem como fazer o CRUD de todos:
+- [x]Será permitida a criação de um usuário com name e username, bem como fazer o CRUD de todos:
 - [] Criar um novo *todo*;
-- [] Listar todos os *todos*;
+- [x] Listar todos os *todos*;
 - [] Alterar o `title` e `deadline` de um *todo* existente;
 - [] Marcar um *todo* como feito;
-- [] Excluir um *todo*;
-- [] Tudo isso para cada usuário em específico (o `username`  será passado pelo header)
+- [x] Excluir um *todo*;
+- [x] Tudo isso para cada usuário em específico (o `username`  será passado pelo header)
--- src/index.js
+++ src/index.js
@@ -12,31 +12,55 @@ const users = [];
 function checksExistsUserAccount(request, response, next) {
   // Complete aqui
   const { username } = request.header;
-  
+  const user = users.find((user) => user.username === username );
+  if(!user){
+        return response.status(400).json(
+            { error: "Username not found" }) ;
+  } 
+  request.user = user
+  next();
 }
 
 app.get("/", (request, response) =>{
   response.json("Todo List").status(200);
-} )
+});
 
 app.post('/users', (request, response) => {
   // Complete aqui
   const {name, username} = request.body;
-  users.push({
+  const userAlreadyExists = users.some(
+      (user) => user.username === username
+  );
+  if(usernameAlreadyExists){
+      return response.status(400).json({ error: "Username already exists" }) 
+  }
+  const user = {
     id: uuidv4,
     name,
     username,
     todos: [] 
-  });
-  return response.status(201).send();  
+  };
+  users.push(user)
+  return response.status(201).json(users);  
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
   // Complete aqui
+  const { username } = request.headers;
+  return response.status(200).json(username.todos)
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
   // Complete aqui
+  const {title, deadline} = req.body;
+  const {user}  = request;
+  const todosOperation = {
+      id: uuidv4, 
+      title,
+      done: false, 
+      deadline,
+      created_at: new Date()
+  }
 });
 
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
@@ -49,6 +73,10 @@ app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
 
 app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
   // Complete aqui
+  const { user } = request;
+  user.splice(user, 1)
+  return response.status(204).json(users)
+
 });
 
 module.exports = app;
